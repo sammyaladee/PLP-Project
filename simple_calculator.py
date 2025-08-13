442e8fba693ab473361084db1ae7414f9dbcf43b
Aladeyelu Olufemi Samuel <151362938+Sammyalade@users.noreply.github.com>
2025-08-13 14:32:04 +0100

    Assignment 2

--- simple_calculator.py
+++ simple_calculator.py
@@ -1,11 +1,8 @@
-# Simple Calculator
 
-# Get user input
 num1 = float(input("Enter the first number: "))
 num2 = float(input("Enter the second number: "))
 operation = input("Enter the operation (+, -, *, /): ")
 
-# Perform the calculation
 if operation == "+":
     result = num1 + num2
 elif operation == "-":
@@ -20,7 +17,6 @@ elif operation == "/":
 else:
     result = "Invalid operation!"
 
-# Display the result
 if isinstance(result, (int, float)):
     print(f"{num1} {operation} {num2} = {result}")
 else:
